93c1dfe86edccd4c63802a89905b97cc279ab4a5
Shutaro Agematsu <Shutaro.agematsu@gmail.com>
2019-06-03 23:11:32 +0900

    軽微なバグ修正

--- Procon30/Game.h
+++ Procon30/Game.h
@@ -11,25 +11,25 @@
 
 // 得点を保持する構造体
 struct GAME_SCORE {
-	int tile_team1;
-	int area_team1;
+	int		tile_team1;
+	int		area_team1;
 
-	int tile_team2;
-	int area_team2;
+	int		tile_team2;
+	int		area_team2;
 }typedef GAME_SCORE;
 
 // フィールドの情報を保持する構造体
 struct BOARD_STATE {
-	int team_ID;		// 本番では変更する
-	int width;			// フィールドの横のマス数 10 ～ 20
-	int height;			// フィールドの縦のマス数 10 ～ 20
-	int turn;			// 試合の全ターン数
-	int now_turn;		// 現在のターン数
-	int agent_qant; // 各チームのエージェントの数 2 ～ 8
-	std::vector<int> tile_score;		// 盤面の得点情報を記憶する配列　サイズは width * height
-	std::vector<int> tile_state;		// タイルの色 0 or 1 or 2
-	std::vector<Agent> agents;			// エージェントたち。1つの配列に無理やり2チーム詰め込んでる
-	GAME_SCORE game_score;
+	int					team_ID;		// 本番では変更する
+	int					width;			// フィールドの横のマス数 10 ～ 20
+	int					height;			// フィールドの縦のマス数 10 ～ 20
+	int					turn;			// 試合の全ターン数
+	int					now_turn;		// 現在のターン数
+	int					agent_count;	// 各チームのエージェントの数 2 ～ 8
+	std::vector<int>	tile_score;		// 盤面の得点情報を記憶する配列　サイズは width * height
+	std::vector<int>	tile_state;		// タイルの色 0 or 1 or 2
+	std::vector<Agent>	agents;			// エージェントたち。1つの配列に無理やり2チーム詰め込んでる
+	GAME_SCORE			game_score;
 }typedef BOARD_STATE;
 
 // 次取る行動を保存する
@@ -42,11 +42,11 @@ struct ACT_STATE {
 
 class Game {
 private:
-	
+	BOARD_STATE board;
 	int getAreaScore(int team);
 	void areaScoreRecursion(int team, int x, int y, std::vector<bool>& table, int& tmp, bool& reach_end);
 	void parse_json(std::string str_json);
-	BOARD_STATE board;
+	
 public:
 	void init();
 
@@ -67,10 +67,8 @@ public:
 	int getNowTurn();
 	int getAgentQant();
 
-
 	std::vector<Agent>& getAgentVector();
 
-	bool updateTurn();	// ターンを移す 試合が終わったらtrueを返す
+	bool updateTurn();				// ターンを移す 試合が終わったらtrueを返す
 	GAME_SCORE countGameScore();	// 得点計算
-
 };
